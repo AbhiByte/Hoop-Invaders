54351abeaea9c43f6ad84b524719ddf2ad7da7ee
AbhiByte <dbossgames20gmail.com>
2021-06-05 19:26:44 -0400

    added features

--- game.py
+++ game.py
@@ -10,9 +10,8 @@ pygame.display.set_caption("cRona-Invaders --alpha 1.00")
 
 #Images
 background_image = pygame.transform.scale(pygame.image.load(os.path.join("Images", "background.jpg")), (WIDTH, HEIGHT))
-hoop_image = pygame.image.load(os.path.join("Images", "hoop.jpg"))
-ball_image = pygame.image.load(os.path.join("Images", "ball.jpg"))
 player_image = pygame.image.load(os.path.join("Images", "playerVirus.png"))
+enemy_image = pygame.image.load(os.path.join("Images", "enemyVirus.png"))
 
 class Virus:
     def __init__ (self, x, y, health=100):
@@ -20,8 +19,8 @@ class Virus:
         self.y = y
         self.health = health
         self.player_image = player_image
-        self.ball_img = None
-        self.balls = []
+        self.enemy_image = enemy_image
+        self.virus_img = enemy_image
         self.coolDownCounter = 0
 
     def draw(self, window):
@@ -36,11 +35,17 @@ class Virus:
 class Player(Virus):
     def __init__ (self, x, y, health=100):
         super().__init__(x, y, health)
-        self.ball_img = ball_image
-        self.mask = pygame.mask.from_surface(self.hoop_image)
+        self.player_image = player_image
+        self.mask = pygame.mask.from_surface(self.player_image)
         self.max_health = health
-#class Enemy(Virus):
-    #def __init__ (self, x, y)
+class Enemy(Virus):
+    def __init__ (self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.virus_image = enemy_image
+        self.mask = pygame.mask.from_surface(self.virus_image)
+
+    def move(self, vel):
+        self.y += vel
 #Main game loop
 def main():
     run = True
@@ -48,8 +53,12 @@ def main():
     clock = pygame.time.Clock()
     mainFont = pygame.font.SysFont("comicsans", 50)
     player_vel = 5
-    level = 1
+    level = 0
     lives = 5
+
+    enemy_vel = 1
+    enemies = []
+    wave_length = 5
 
     player = Virus(300, 650)
     def redraw_window():
@@ -61,12 +70,23 @@ def main():
         WIN.blit(livesLabel, (10, 10))
         WIN.blit(levelLabel, (WIDTH - levelLabel.get_width() - 10, 10))
 
+        for enemy in enemies:
+            enemy.draw(WIN)
+
         player.draw(WIN)
+
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
+
+        if len(enemies) == 0:
+            level += 1
+            wave_length += 5
+            for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH-100), random.randrange(-1500, -100))
+                enemies.append(enemy)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -79,6 +99,13 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
             player.y += player_vel
+
+        for enemy in enemies[:]:
+            enemy.move(enemy_vel)
+            if enemy.y + enemy.get_height() > HEIGHT:
+                lives -= 1
+                enemies.remove(enemy)
+        redraw_window()
     pygame.quit()
 
 
