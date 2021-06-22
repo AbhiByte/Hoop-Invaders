99225658cbf3579fbcb4d0a246a6d94a1216560f
AbhiByte <dbossgames20@gmail.com>
2021-06-21 21:23:21 -0400

    Finished game

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("cRona-Invaders --alpha 1.00")
 
 #Images
-background_image = pygame.transform.scale(pygame.image.load(os.path.join("Images", "background.jpg")), (WIDTH, HEIGHT))
+background_image = pygame.transform.scale(pygame.image.load(os.path.join("Images", "backg.png")), (WIDTH, HEIGHT))
 player_image = pygame.image.load(os.path.join("Images", "playerVirus.png"))
 enemy_image = pygame.image.load(os.path.join("Images", "enemyVirus.png"))
 laser_blue_image = pygame.image.load(os.path.join("Images", "pixel_laser_blue.png"))
@@ -34,15 +34,14 @@ class Laser:
     def move(self, vel):
         self.y += vel
     def offTheScreen(self, height):
-        return self.y <= height and self.y >= 0
+        return not (self.y <= height and self.y >= 0)
+        #return obj1.mask.overlap(obj2, (offset_x, offset_y)) != None
     def collision(self, obj):
-        return collide(obj, self)
-def collide(obj1, obj2):
-    offset_x = obj2.x - obj1.x
-    offset_y = obj2.y - obj1.y
-    return obj1.mask.overlap(obj2, (offset_x, offset_y))
+        return collide(self, obj)
 
 class Virus:
+    COOLDOWN = 30
+
     def __init__ (self, x, y, health=100):
         self.x = x
         self.y = y
@@ -50,12 +49,35 @@ class Virus:
         self.player_image = player_image
         self.enemy_image = enemy_image
         self.virus_img = enemy_image
-        self.coolDownCounter = 0
-        self.laser_img = None
+        self.cool_down_counter  = 0
+        self.laser_img = laser_red_image
         self.lasers = []
 
     def draw(self, window):
         window.blit(self.player_image, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+    def move_lasers (self, vel, obj):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.offTheScreen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
+
+    def cooldown(self):
+        if self.cool_down_counter >= self.COOLDOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
 
     def get_width(self):
         return self.player_image.get_width()
@@ -69,6 +91,17 @@ class Player(Virus):
         self.player_image = player_image
         self.mask = pygame.mask.from_surface(self.player_image)
         self.max_health = health
+    def move_lasers (self, vel, objs):
+        self.cooldown()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.offTheScreen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj):
+                        objs.remove(obj)
+                        self.lasers.remove(laser)
 class Enemy(Virus):
     def __init__ (self, x, y, health=100):
         super().__init__(x, y, health)
@@ -77,6 +110,11 @@ class Enemy(Virus):
 
     def move(self, vel):
         self.y += vel
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y))
 #Main game loop
 def main():
     run = True
@@ -85,6 +123,7 @@ def main():
     mainFont = pygame.font.SysFont("comicsans", 50)
     lost_font = pygame.font.SysFont("comicsans", 60)
     player_vel = 5
+    laser_vel = 4
     level = 0
     lives = 5
 
@@ -149,12 +188,16 @@ def main():
             player.y -= player_vel
         if keys[pygame.K_s] or keys[pygame.K_DOWN] and player.y + player_vel + player.get_height() < HEIGHT:
             player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
 
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
+            enemy.move_lasers(laser_vel, player)
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
+        player.move_lasers(laser_vel, enemies)
         #redraw_window()
     pygame.quit()
 
@@ -163,7 +206,7 @@ def home_screen():
     runn = True
     FPS = 60
     clock = pygame.time.Clock()
-    mainFont = pygame.font.SysFont("comicsans", 50)
+    mainFont = pygame.font.SysFont("comicsans", 100)
 
     WIN.blit(background_image, (0, 0))
     #Home screens texts
@@ -171,25 +214,31 @@ def home_screen():
     instructions_text = "How To Play"
     credits_text = "Credits"
     #Creating home screen labels
-    startLabel = mainFont.render(start_text, 1, (255,0,0))
+    startLabel = mainFont.render(start_text, 1, (0,255,0))
     instructionsLabel = mainFont.render(instructions_text, 1, (0, 255, 0))
     creditsLabel = mainFont.render(credits_text, 1, (0, 255, 0))
 
     #Bliting to screen
-    WIN.blit(startLabel, (WIDTH/2 - 100, HEIGHT/2))
-    WIN.blit(instructionsLabel, (WIDTH/2 - 105, HEIGHT/2 + 100))
-    WIN.blit(creditsLabel, (WIDTH/2 - 70, HEIGHT/2 + 200))
+    WIN.blit(startLabel, (WIDTH/2 - startLabel.get_width()/2, HEIGHT/2 - 100))
+    WIN.blit(instructionsLabel, (WIDTH/2 - instructionsLabel.get_width()/2, HEIGHT/2))
+    WIN.blit(creditsLabel, (WIDTH/2 - creditsLabel.get_width()/2, HEIGHT/2 + 100))
+
+    mouse = pygame.mouse.get_pos()
     while runn:
         clock.tick(FPS)
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 runn = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if mouse[1] <= WIDTH/2:
+                    main()
     if runn == False:
         pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    #main()
 
-    #home_screen()
+    home_screen()
